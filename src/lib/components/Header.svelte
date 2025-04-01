967a2165d220b199aa211eefc0a9262018d5ad92
Alex Burgos <alexander.burgos1@gmail.com>
2025-04-01 10:05:28 +0200

    Add margin top to header on mobile

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <h1
-  class="mb-20 text-center text-[4rem] font-bold tracking-tight sm:text-[6rem] md:mb-24 md:text-[10rem] lg:text-[14rem]"
+  class="mt-10 mb-20 text-center text-[4rem] font-bold tracking-tight sm:text-[6rem] md:mt-0 md:mb-24 md:text-[10rem] lg:text-[14rem]"
 >
   {title}
 </h1>
