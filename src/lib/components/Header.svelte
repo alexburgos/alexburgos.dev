acbc06b0ea0a0ddaf59d9f3db9e04ebce7be4dc9
Alex Burgos <alexander.burgos1@gmail.com>
2025-07-22 09:01:24 -0400

    fix: increase size of heading

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -77,7 +77,7 @@
 
 <div class="relative mt-10 mb-20 md:mt-0 md:mb-24">
   <h1
-    class="cursor-pointer text-center text-[10rem] font-bold tracking-tight hover:text-red-800 sm:text-[6rem]"
+    class="cursor-pointer text-center text-[4rem] font-bold tracking-tight hover:text-red-800 sm:text-[6rem] lg:text-[10rem]"
   >
     {title}
   </h1>
