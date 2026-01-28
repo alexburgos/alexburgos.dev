dc070f2ca5e775e0856cb82c265774626d4e2baa
Alexander Burgos <alexander.burgos@kiwi.com>
2026-01-28 22:39:24 +0100

    added scarpeltele to projects

--- src/lib/components/Projects.svelte
+++ src/lib/components/Projects.svelte
@@ -4,21 +4,21 @@
   //ts-ignore
   let carousel: HTMLElement | null = null;
 
-  // function scrollPrev() {
-  //   if (!carousel) return;
-  //   const item = carousel.querySelector('.carousel-item') as HTMLElement;
-  //   if (!item) return;
-  //   const scrollAmount = item.offsetWidth + 32;
-  //   carousel.scrollBy({ left: -scrollAmount, behavior: 'smooth' });
-  // }
+  function scrollPrev() {
+    if (!carousel) return;
+    const item = carousel.querySelector('.carousel-item') as HTMLElement;
+    if (!item) return;
+    const scrollAmount = item.offsetWidth + 32;
+    carousel.scrollBy({ left: -scrollAmount, behavior: 'smooth' });
+  }
 
-  // function scrollNext() {
-  //   if (!carousel) return;
-  //   const item = carousel.querySelector('.carousel-item') as HTMLElement;
-  //   if (!item) return;
-  //   const scrollAmount = item.offsetWidth + 32;
-  //   carousel.scrollBy({ left: scrollAmount, behavior: 'smooth' });
-  // }
+  function scrollNext() {
+    if (!carousel) return;
+    const item = carousel.querySelector('.carousel-item') as HTMLElement;
+    if (!item) return;
+    const scrollAmount = item.offsetWidth + 32;
+    carousel.scrollBy({ left: scrollAmount, behavior: 'smooth' });
+  }
 </script>
 
 <div class="relative mx-auto mb-20 px-6 lg:mb-28 lg:w-[912px] lg:px-0" id="projects">
@@ -50,8 +50,17 @@
         projectLink="https://www.uniondiscount.net"
       />
     </div>
+    <div class="carousel-item w-[85vw] md:w-[280px]">
+      <ProjectCard
+        project="Scarpeltele"
+        role="Developer"
+        company="Scarpel Limited"
+        location="Remote"
+        projectLink="https://scarpeltele.com/"
+      />
+    </div>
   </div>
-  <!-- <button
+  <button
     onclick={scrollPrev}
     aria-label="Previous project"
     class="absolute top-1/2 left-[-60px] hidden -translate-y-1/2 transform cursor-pointer transition-opacity hover:opacity-75 md:block"
@@ -70,8 +79,8 @@
       <line x1="19" y1="12" x2="5" y2="12"></line>
       <polyline points="12 19 5 12 12 5"></polyline>
     </svg>
-  </button> -->
-  <!-- <button
+  </button>
+  <button
     onclick={scrollNext}
     aria-label="Next project"
     class="absolute top-1/2 right-[-60px] hidden -translate-y-1/2 transform cursor-pointer transition-opacity hover:opacity-75 md:block"
@@ -90,5 +99,5 @@
       <line x1="5" y1="12" x2="19" y2="12"></line>
       <polyline points="12 5 19 12 12 19"></polyline>
     </svg>
-  </button> -->
+  </button>
 </div>
