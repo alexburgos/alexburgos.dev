cd9fdc61697c546599aef691f2f34c993a652e2a
Alex Burgos <alexander.burgos1@gmail.com>
2026-04-14 15:33:41 +0200

    fixing TS issues in Links

--- src/lib/components/Links.svelte
+++ src/lib/components/Links.svelte
@@ -1,9 +1,15 @@
 <script lang="ts">
-  const links = [
+  interface Link {
+    text: string;
+    href: string;
+    external?: boolean;
+  }
+
+  const links: Link[] = [
     { text: 'Blog', href: '/blog' },
     { text: 'Projects', href: '/projects' },
-    { text: 'Github', href: 'https://github.com/alexburgos' },
-    { text: 'Linkedin', href: 'https://www.linkedin.com/in/alexander-b-67073129' },
+    { text: 'Github', href: 'https://github.com/alexburgos', external: true },
+    { text: 'Linkedin', href: 'https://www.linkedin.com/in/alexander-b-67073129', external: true },
   ];
 </script>
 
@@ -11,8 +17,8 @@
   {#each links as link (link.text)}
     <a
       href={link.href}
-      target={link.target}
-      rel={link.rel}
+      target={link.external ? '_blank' : undefined}
+      rel={link.external ? 'noopener noreferrer' : undefined}
       class="text-sm transition-colors hover:text-red-800 md:leading-6">{link.text}</a
     >
   {/each}
